4f0dcc7c380ecc0e54e09bb80e2c0d1c8dc94e77
Nathaniel Craan <45303361+craannj@users.noreply.github.com>
2021-11-09 00:38:29 -0500

    Update temp.py

--- hw09/temp.py
+++ hw09/temp.py
@@ -45,7 +45,7 @@ BUS="i2c-1"
 ADDR="0x40"
 siPATH="/sys/class/i2c-adapter/" + BUS + "/1-0040/iio:device1/"
 tmpPATH="/sys/class/hwmon/hwmon0/"
-sleepTime=5
+sleepTime=60*60
 
 def read_temp_raw(device_file):
     f = open(device_file, 'r')
